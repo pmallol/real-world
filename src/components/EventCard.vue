09d18b9466b6ee6918fc43e4276b5786135418a6
pmallol <paulamallol@gmail.com>
2024-05-11 18:05:34 -0300

    refactor: Update EventCard component to display event details
    The EventCard component has been updated to display the title and date of the event.

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,12 +1,31 @@
 <script setup>
-
+import {ref} from 'vue'
+const event = ref({
+  title: 'Vue.js Global Summit',
+  date: 'September 2021',
+  location: 'Online'
+})
 </script>
 
 <template>
-  <div class="greetings">
+  <div class="event-card">
+    <h2>{{ event.title }}</h2>
+    <span>on {{ event.date }}</span>
   </div>
 </template>
 
 <style scoped>
+.event-card {
+  width: 350px;
+  padding: 20px;
+  cursor: pointer;
+  border: solid 1px #5784b2;
+  margin-bottom: 18px;
+}
 
+.event-card:hover {
+  background-color: #f0f0f0;
+  transform: scale(1.02);
+  box-shadow: 0 3px 12px 0 rgba(0,0,0,0.2);
+}
 </style>
